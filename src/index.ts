346297ff00655ba9e0acbc9757fb508cb20a9abf
Mohini Jayant Pathak <mohinijayant.pathak@ad.infosys.com>
2023-12-11 13:17:50 +0530

    Setting up git actions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { getInput } from "@actions/core"
+import { context } from "@actions/github"
+import { Context } from "@actions/github/lib/context"
+import { type } from "os"
+
+type GithubContext = typeof context;
 
 const inputName = getInput("name")
 
-greet(inputName)
+greet(inputName, getRepoUrl(context));
 
-function greet (name: String) {
-    console.log(`'Hello, ${name}!'`)
+function greet (name: String, repoUrl: String) {
+    console.log(`'Hello, ${name}! You are running GitHub actions in ${repoUrl}'`)
+}
+
+function getRepoUrl({repo, serverUrl}: GithubContext): string {
+    return `${serverUrl}/${repo.owner}/${repo.repo}`
 }
